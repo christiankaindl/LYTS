5df843ef47fc726a62540ac71d0adb57fd8d9fc8
Christian Kaindl <christian.kaindl@hey.com>
2022-01-12 11:56:40 +0100

    Add library setup
    https://vitejs.dev/guide/build.html#library-mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,34 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tsconfigPaths({
-      root: '.'
-    }),
+    tsconfigPaths({ root: '.' }),
     vanillaExtractPlugin(),
     react(),
+    dts()
   ],
-  resolve: {
-    alias: {
-      "react/jsx-runtime": "react/jsx-runtime.js"
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'lyts',
+      fileName: (format) => `lyts.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React'
+        }
+      }
     }
   }
 })
