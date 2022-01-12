96201f96b4687b7b2a03b5d276a9c4ce103df352
Christian Kaindl <christian.kaindl@hey.com>
2022-01-12 11:12:31 +0100

    Re-export components in index file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
-import { CSSProperties } from "@vanilla-extract/css"
-
 export const lytsId = Symbol('Identifier for LYTS React components')
-
-export interface BaseProps {
-  /** Amount of space between elements */
-  gap?: CSSProperties['gap']
-  /** Inspired by Radix UI's API */
-  asChild?: boolean
-  /** Amount of space between the border and the content */
-  padding?: CSSProperties['padding']
-  /** Visually break out of the parent's box. Useful for visually aligning e.g. transparent buttons */
-  bleed?: CSSProperties['padding']
-  bleedTop?: CSSProperties['padding']
-  bleedRight?: CSSProperties['padding']
-  bleedBottom?: CSSProperties['padding']
-  bleedLeft?: CSSProperties['padding']
-}
+export * from '@lib/Box/Box'
+export * from '@lib/Stack/Stack'
+export * from '@lib/Row/Row'
+export * from '@lib/Clamp/Clamp'
+export * from '@lib/Columns/Columns'
+export * from '@lib/Grid/Grid'
