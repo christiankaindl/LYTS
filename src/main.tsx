cf0e8bd0e4cbe25d2c0ee6d27220df7f692622e4
Christian Kaindl <christian.kaindl@hey.com>
2022-01-12 11:51:02 +0100

    Add simple debug page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import ReactDOM from 'react-dom'
-// import './index.css'
-import App from '../docs/App'
+import { Stack, Row, Clamp, Columns, Grid } from './'
+
+const App: FunctionComponent = function () {
+  return (
+    <div>
+      <Stack>
+        <span>Item 1</span>
+        <span>Item 2</span>
+        <span>Item 3</span>
+      </Stack>
+      <hr />
+      <Row>
+        <span>Item 1</span>
+        <span>Item 2</span>
+        <span>Item 3</span>
+      </Row>
+      <hr />
+      <Clamp clamp='900px'>
+        <span>Child</span>
+      </Clamp>
+      <hr />
+      <Columns>
+        <span>Item 1</span>
+        <span>Item 2</span>
+        <span>Item 3</span>
+      </Columns>
+      <hr />
+      <Grid>
+        <span>Item 1</span>
+        <span>Item 2</span>
+        <span>Item 3</span>
+        <span>Item 1</span>
+        <span>Item 2</span>
+        <span>Item 3</span>
+      </Grid>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
