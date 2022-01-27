76598011c42741c936a7672c29f084c44d4696fa
Christian Kaindl <christian.kaindl@hey.com>
2022-01-27 20:08:20 +0100

    cleanup(Split): Remove unused styles

--- src/Split/Split.css.ts
+++ src/Split/Split.css.ts
@@ -1,23 +1,9 @@
-import { fallbackVar, globalStyle, style } from '@vanilla-extract/css'
-import { lyts, vars } from '../index.css'
+import { style } from '@vanilla-extract/css'
+import { lyts } from '../index.css'
 
 export const split = style([
   lyts,
   {
-    display: 'grid',
-    gridTemplateColumns: '1fr auto 1fr',
-    gap: fallbackVar(vars.gap, '1em'),
-    flexGrow: 1,
-    alignItems: 'center'
+    flexGrow: 1
   }
 ])
-globalStyle(`${split} > *:first-child`, {
-  justifySelf: 'start'
-})
-globalStyle(`${split} > *:last-child`, {
-  justifySelf: 'end'
-})
-
-export const twoChildren = style({
-  gridTemplateColumns: '1fr 1fr'
-})
