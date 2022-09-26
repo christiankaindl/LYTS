712ff4ba0ae7ceecfd5b74521b2869ab5803027a
Christian Kaindl <christian.kaindl@hey.com>
2022-09-26 23:35:48 +0200

    DebugProvider: Fix up children prop type

--- src/DebugProvider/DebugProvider.tsx
+++ src/DebugProvider/DebugProvider.tsx
@@ -1,11 +1,11 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, PropsWithChildren } from "react";
 import { debug } from './DebugProvider.css'
 
 /**
  * Render all children LYTS components with extra visual cues
  * @private
  */
-const DebugProvider: FunctionComponent = function DebugProvider ({ children }) {
+const DebugProvider: FunctionComponent<PropsWithChildren> = function DebugProvider ({ children }) {
   return (
     <div className={debug}>
       {children}
