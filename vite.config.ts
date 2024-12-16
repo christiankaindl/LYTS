a27c6d734bda44171e5313c6feb9510c02a38549
Christian Kaindl <christiankaindl@users.noreply.github.com>
2024-12-16 12:08:59 +0100

    vite.config.ts: Externalize "react/jsx-runtime"
    Reduces bundle by half, improves interopability

--- vite.config.ts
+++ vite.config.ts
@@ -32,14 +32,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'react-dom'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React'
-        }
-      }
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
     }
   }
 })
